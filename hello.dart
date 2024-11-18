686f5a150437c5d495662d1481e1693bef2204f8
phunyaphat65 <phunyapatp65@nu.ac.th>
2024-11-18 15:12:51 +0700

    Initial commit

---
+++ hello.dart
@@ -0,0 +1,10 @@
+void main() {
+  int num1 = 10; // declaring number1
+  int num2 = 3; // declaring number2
+  
+  int quotient = num1 ~/ num2;
+  int remainder = num1 % num2; 
+  
+  print("Quotient = $quotient");
+  print("Remainder = $remainder");
+}
